985d4466ebd1c88d1f45432d418f87dfd7392c03
PerrottLT <lukeperrott@gmail.com>
2023-07-12 21:56:41 +0100

    Module 1 of introduction to R
    Datacamp, Introduction to R, module 1.
    Contents:
    Basic arithmetic
    Variable assignment
    Datatypes

---
+++ Intro - Basics.R
@@ -0,0 +1,86 @@
+# R makes use of the # sign to add comments, so that you and others can understand what the R code is about.
+# Comments are not run as R code, so they will not influence your result.
+
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6 + 12
+
+#Arithmetic with R
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulo
+28 %% 6
+
+#Variable assignment
+
+#A basic concept in (statistical) programming is called a variable. 
+
+#A variable allows you to store a value (e.g. 4) or an object (e.g. a function description) in R. You can then later use this variable's name to easily access the value or the object that is stored within this variable. 
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+#print my_fruit
+my_fruit
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+#What's that data type?
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
+
+
